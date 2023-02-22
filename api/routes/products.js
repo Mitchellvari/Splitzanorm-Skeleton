4152d6d334a44c1bba122d161d3112584cffcf20
Mitchell Vari <mitchell.vari@developersinstitute.co.nz>
2023-02-23 10:39:35 +1300

    updated price in sql file

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,12 +5,9 @@ const stripe = require("stripe")(
   "sk_test_51LLga0Lh8BBWwJ2do9sDSkHrSDO2nMTXkWVGEpK6CutJH84yLKSaZGNoNvziqhV2Dls1nxdPVoyh66QH6FDVKv4I00vciEpFP7"
 );
 
-
-
-
 router.get("/products", async function (req, res, next) {
-const data = await db.query("SELECT * FROM products")
-const output = data.rows
+  const data = await db.query("SELECT * FROM products");
+  const output = data.rows;
   res.status(200).send(output);
 });
 
